e3ccd02ab64466cab7776c825f9eb3229dfb3fda
Cathal Mooney <topranks@github.com>
2026-05-25 16:30:37 +0100

    updated add_fqdn_hosts for new clab hosts format

--- add_fqdn_hosts.py
+++ add_fqdn_hosts.py
@@ -9,10 +9,12 @@ def main():
 
     with open('/etc/hosts', 'r') as hostsfile:
         for line in hostsfile.readlines():
-            line_strip = line.rstrip('\n')
-            if "lab-" in line:
-                device = line.split()[-1].split("-")[-1]
-                new_file.write(f"{line_strip}\t{device}\n")
+            line_strip = line.strip()
+            if "lab-" in line and not ("START" in line or "END" in line):
+                description = line.split("#")[-1]
+                host_data = line.split("#")[0]
+                device = host_data.split()[1].split("-")[-1]
+                new_file.write(f"{host_data} {device}\t#{description}")
             else:
                 new_file.write(line)
 
